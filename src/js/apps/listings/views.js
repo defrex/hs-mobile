6cf2521bd83474a3301a1159597ca12f7cbd8d97
defrex <defrex@gmail.com>
2011-04-18 15:13:24 -0400

    posting from app not operable

--- src/js/apps/listings/views.js
+++ src/js/apps/listings/views.js
@@ -4,6 +4,7 @@ goog.provide('hs.listings.views');
 goog.require('hs.tmpl.listings');
 goog.require('frame.View');
 goog.require('frame.apiRequest');
+goog.require('hs.listings.fakeImage');
 
 /** @constructor **/
 hs.listings.views.Add = function(){
@@ -54,20 +55,23 @@ hs.listings.views.Add.prototype.enterDocument = function(){
     this.doc.q('#take_image').on('click', function(e){
         var v = this;
         frame.log('getting image');
-
         frame.log('nav.cam ' + typeof navigator.camera);
-        navigator.camera.getPicture(function(image){
+
+        var withImage = function(image){
             //v.doc.q('#image').attr('src', image);
             v.doc.q('#image').attr('src', 'data:image/jpeg;base64,'+image);
             v.doc.q('label[for="take_image"]').hide();
             v.imageData = image;
             v.doc.q('#take_image').attr('value', 'Retake');
-        }, function(){
-            frame.log('#fail');
-        }, {
-            quality: 10,
-            //destinationType: Camera.DestinationType.FILE_URI
-        });
+        };
+
+        if (typeof navigator.camera != 'undefined')
+            navigator.camera.getPicture(withImage, function(){
+                frame.log('navigator.camera #fail');
+                withImage(hs.listings.fakeImage);
+            }, {quality: 10});
+        else
+            withImage(hs.listings.fakeImage);
     }, this);
 
     // fastbutton these focus clicks
@@ -99,7 +103,8 @@ hs.listings.views.Add.prototype.submit = function(){
         'price': this.doc.q('#price').val(),
         'latitude': '43.6519',
         'longtitude': '-79.3736',
-        'photo': this.imageData
+        'photo': this.imageData,
+        'user': frame.store.get('userURI')
     };
     frame.log('submitting');
     frame.apiRequest({method: 'POST', path: '/api/v1/listing/', body: data},
