1208a62268f7de17928e72bb18858a290d28c5c3
defrex <defrex@gmail.com>
2011-04-21 17:38:47 -0400

    change server to dev2

--- src/js/init.js
+++ src/js/init.js
@@ -7,7 +7,7 @@ goog.require('hs.urls');
 setTimeout(function(){
     frame.start({
         urls: hs.urls,
-        apiServer: 'dev.hipsell.com',
+        apiServer: 'dev2.hipsell.com',
         on403: function(){
             this.clear();
             this.goTo('/login/');
